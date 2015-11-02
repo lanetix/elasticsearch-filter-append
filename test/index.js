18ef0763a43986529f55e6dc170b77f64ad61512
Antwan Wimberly <antwan.wimberly@gmail.com>
2015-11-01 20:41:08 -0800

    migrate input and expectation to external file

--- test/index.js
+++ test/index.js
@@ -3,34 +3,10 @@ var should = require('should')
 
 describe('filter append', function() {
   it('should convert a query to a filtered query', function() {
-    var search = {
-      query: {
-        query_string: {
-          query: '*I spy an @kyspy \? \* &*',
-          default_operator: 'AND',
-          fields: ['name', 'first_name', 'workflow.owner.name']
-        }
-      }
-    }
+    var search = require('./scenarios/query.json').search
+    var filter = require('./scenarios/query.json').filter
+    var output = require('./scenarios/query.json').output
 
-    var filter = {
-      term: {
-        name: "Antwan Atkins"
-      }
-    }
-
-    var output = {
-      query: {
-        filtered: {
-          query: search.query,
-          filter: {
-            bool: {
-              should: [filter]
-            }
-          }
-        }
-      }
-    }
     var builder = new FilterBuilder(search)
 
     builder.append(filter)
